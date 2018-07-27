c61a32904d98285d159dd2aa09e8d8e6bd9c6cd1
SA <shivaram@gpsl.co>
2018-07-27 15:49:52 +0530

    links

--- send_email.php
+++ send_email.php
@@ -8,7 +8,5 @@
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-    mail($to,$subject,$message,$headers);
-    
-    return;
+    return mail($to,$subject,$message,$headers);  
 ?>
