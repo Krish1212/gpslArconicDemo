f3b5334ed9e4529053f826f325887002672d45d4
sri <srividhya@gpsl.co>
2018-08-03 11:15:51 +0530

    included pdf search. It searches only one occurrence in the pdf.

--- searchdata.php
+++ searchdata.php
@@ -1,36 +1,43 @@
-<?php 
-  $pagetitle = "Arconic Service Application | GPSL | Plant Map";
-  include_once('header.php');
-?>
-<body>
-    <script>whereami = 'plant';</script>
-    <?php 
-        $navtitle = "Service Application Manager";
-        include_once('navbar.php');
-    ?>
-
-<div style="width:80%;margin:0 auto;margin-top:18px;">
-<h2>Search Result</h>
-
 <?php
 $filepath = 'data/comments.json';
-$data = file_get_contents($filepath); // put the contents of the file into a variable
-$datacollection = json_decode($data, true); // decode the JSON feed
+$data = file_get_contents($filepath); 
+$datacollection = json_decode($data, true); 
 $count = count($datacollection);
 $urlpath = '';
-$displayresult = '';
-$search = $_GET ['search'];
-echo "<div style='border:2px solid #01B0BA;background-color:#ebf3f4;padding:20px 20px 20px 20px;'>";
 $x='';
+$displayresult = '';
+include ( 'PdfToText-master/PdfToText.phpclass' ) ;
+$pdfpath = 'downloads/pll_2373.pdf';
+$pdf	=  new PdfToText ( $pdfpath ) ;	
+$search = $_GET ['search'];
+//$searchuc = ucwords($search);
+//$searchlc = lcfirst($search);
+$pdfdata = $pdf -> Text ;
+if( stripos( $pdfdata, $search) !== false){
+$pos = stripos($pdfdata, $search);
+$previous = substr($pdfdata,0,$pos);
+$next = substr($pdfdata,$pos);
+if (strlen($previous) > 100){
+	$previous = substr($previous,strlen($previous) - 100);
+}
+if (strlen($next) > 100){
+	$next = substr($next,0,100);
+}
+$pdfresult = $previous." ".$next;
+$pdfresult = str_ireplace("$search","<span style='color:white;font-weight:bold;background-color:#01B0BA;'>$search</span>",$pdfresult);
+$displayresult.="<span style='font-family:sans-serif;'/>$pdfresult<br><a href='$pdfpath'>$pdfpath</a><br><br>";
+$x++;
+}
+echo "<div style='border:2px solid #01B0BA;background-color:#ebf3f4;padding:20px 20px 20px 20px;'>";
 foreach($datacollection as $key => $value)
 {
 	$urlpath = ($value['url']);
 		foreach($value as $keys =>$values){
 			if(isset($values['data'])){
 				$searchdata = $values['data'];
-				if( strpos( $searchdata, $search ) !== false){
+				if( stripos( $searchdata, $search ) !== false){
 				$x++;
-				$searchdata = str_replace("$search","<span style='color:white;font-weight:bold;background-color:#01B0BA;'>$search</span>",$searchdata);
+				$searchdata = str_ireplace("$search","<span style='color:white;font-weight:bold;background-color:#01B0BA;'>$search</span>",$searchdata);
 				$displayresult .= "				
 				<span style='font-family:sans-serif;'/>$searchdata<br>
 				<a href='$urlpath'>$urlpath</a><p>
@@ -48,7 +55,3 @@ else{
 }
 echo "</div>";
 ?>
-
-</div>
-</body>
-</html>
