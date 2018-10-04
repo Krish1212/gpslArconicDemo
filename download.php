cf879ff128b2eac30bc54f2b0cff7f45bcf35043
Krish1212 <mailkimu@gmail.com>
2018-10-04 13:19:23 +0530

    Added download link for work order from view cart page Added the work order template file inside downloads folder

--- download.php
+++ download.php
@@ -1,4 +1,20 @@
 <?php
+    $file = basename($_GET['file']);
+    $path = './downloads/' . $file;
+
+    if(!file_exists($path)){ // file does not exist
+        die('file not found');
+    } else {
+        header("Cache-Control: public");
+        header("Content-Description: File Transfer");
+        header("Content-Disposition: attachment; filename=$file");
+        header("Content-Type: application/zip");
+        header("Content-Transfer-Encoding: binary");
+
+        // read the file from disk
+        readfile($file);
+    }
+/*
     $contents = $_GET['content'];
     // echo $contents;
     header("Content-Type: text/csv");
@@ -8,7 +24,7 @@
     header('Cache-Control: no-cache');
     header('Pragma: no-cache');
     header('Content-Length: ' . strlen($contents));
-    echo $contents;
+    echo $contents;*/
     $complete = 1;
     exit;
 ?>
