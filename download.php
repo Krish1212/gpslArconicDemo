df435d9e1b04dd8da45a3c29ffd677b3d994cde3
Krish1212 <mailkimu@gmail.com>
2018-10-05 10:27:01 +0530

    Modified header attributes to check if the error is fixed or not

--- download.php
+++ download.php
@@ -19,13 +19,14 @@
         if(!file_exists($path)){ // file does not exist
             die('file not found');
         } else {
-            header("Content-Type: application/ms-excel");
-            header("Content-Description: File Transfer");
-            header("Content-Disposition: attachment; filename=$file");
-            header('Expires: 0');
-            header('Cache-Control: no-cache');
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            header('Content-Disposition: attachment; filename=work_order_template.xlsx');
+            header('Content-Transfer-Encoding: binary');
+            header('Cache-Control: post-check=0, pre-check=0', false);
+            header('Cache-Control: no-store, no-cache, must-revalidate');
             header('Pragma: no-cache');
-            header("Content-Length: " . filesize($file));
+            header('Content-Length: '.$filesize($path));
 
             // read the file from disk
             //readfile($file);
